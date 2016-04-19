367d55abee03442c1c0a3f383ed5a7b92db79fa6
YiuChoi <whucaiyao@gmail.com>
2016-04-19 20:13:49 +0800

    测试修改

--- app/src/main/java/name/caiyao/tencentsport/SettingFragment.java
+++ app/src/main/java/name/caiyao/tencentsport/SettingFragment.java
@@ -8,7 +8,7 @@ import android.preference.EditTextPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
-public class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
+public class SettingFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
     EditTextPreference editTextPreference;
     public final String SETTING_CHANGED = "name.caiyao.tencentsport.SETTING_CHANGED";
@@ -20,41 +20,39 @@ public class SettingFragment extends PreferenceFragment implements Preference.On
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getPreferenceManager().setSharedPreferencesMode(1);
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
         addPreferencesFromResource(R.xml.preference);
-//        getPreferenceManager().getSharedPreferences().edit()
-//                .putBoolean("weixin", true)
-//                .putBoolean("qq", true)
-//                .putString("magnification", "1000")
-//                .apply();
-//        getKey();
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        findPreference("weixin").setOnPreferenceChangeListener(this);
-        findPreference("qq").setOnPreferenceChangeListener(this);
+        //findPreference("weixin").setOnPreferenceChangeListener(this);
+        //findPreference("qq").setOnPreferenceChangeListener(this);
         editTextPreference = (EditTextPreference) findPreference("magnification");
-        editTextPreference.setOnPreferenceChangeListener(this);
+        //editTextPreference.setOnPreferenceChangeListener(this);
         changeSummary();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         changeSummary();
-       // Toast.makeText(getActivity(), "重启将恢复默认值！", Toast.LENGTH_SHORT).show();
         getKey();
         return true;
     }
 
     public void getKey() {
-        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
-        //为了设置实时生效
         Intent intent = new Intent(SETTING_CHANGED);
         getActivity().sendBroadcast(intent);
     }
 
     private void changeSummary() {
         editTextPreference.setSummary(getPreferenceManager().getSharedPreferences().getString("magnification", "1000"));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        changeSummary();
+        getKey();
     }
 }
