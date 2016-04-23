8425d887a7f07c89af85ff9cadf2dee29ef238f8
YiuChoi <whucaiyao@gmail.com>
2016-04-23 17:03:39 +0800

    添加其他运动软件

--- app/src/main/java/name/caiyao/tencentsport/SettingFragment.java
+++ app/src/main/java/name/caiyao/tencentsport/SettingFragment.java
@@ -52,7 +52,8 @@ public class SettingFragment extends PreferenceFragment implements Preference.On
     }
 
     private void changeSummary() {
-        mEditTextPreference.setSummary(getPreferenceManager().getSharedPreferences().getString("magnification", "1000"));
+        if (mEditTextPreference != null)
+            mEditTextPreference.setSummary(getPreferenceManager().getSharedPreferences().getString("magnification", "1000"));
     }
 
     @Override
