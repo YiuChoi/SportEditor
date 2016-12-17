469e33f64e4b615855c43b1bc0d8c346e1f035a4
蔡小木 <whucaiyao@gmail.com>
2016-12-17 12:55:52 +0800

    25.0.1

--- app/src/main/java/name/caiyao/tencentsport/MainActivity.java
+++ app/src/main/java/name/caiyao/tencentsport/MainActivity.java
@@ -5,7 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+
+import org.json.JSONArray;
+
+import java.util.List;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
+import cn.bmob.v3.listener.QueryListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +27,24 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        Bmob.initialize(this, "a898e56e1f6df57e385ade8bb38452d7");
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                BmobQuery<JdAd> bmobQuery = new BmobQuery<JdAd>();
+                bmobQuery.setLimit(5);
+                bmobQuery.findObjectsByTable(new QueryListener<JSONArray>() {
+                    @Override
+                    public void done(JSONArray jsonArray, BmobException e) {
+                        if (e== null){
+                            Log.i("bmob","查询成功："+jsonArray.toString());
+                        }else{
+                            Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                        }
+                    }
+                });
             }
         });
     }
