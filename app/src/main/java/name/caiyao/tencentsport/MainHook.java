3817de4a1ce2ca54669df1af90ba4ca44f60f246
蔡小木 <whucaiyao@gmail.com>
2016-12-20 16:17:13 +0800

    25.0.1

--- app/src/main/java/name/caiyao/tencentsport/MainHook.java
+++ app/src/main/java/name/caiyao/tencentsport/MainHook.java
@@ -30,15 +30,10 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
     private static final String PINGAN = "com.pingan.papd";
     private static final String CODOON = "com.codoon.gps";
     private static final String WEIBO = "com.sina.weibo";
-    private static Handler handler = new Handler();
     Context systemContext;
-    private static int weixinCount = 0, qqCount = 0, ledongCount = 0, yuedongCount = 0, pinganCount = 0, codoonCount = 0;
-    private static float count = 0;
-    private static boolean isWeixin, isQQ, isAuto, isLedong, isYuedong, isPingan, isCodoon, isWeibo;
-    private XSharedPreferences sharedPreferences;
-    private static int m, max = Integer.MAX_VALUE;
 
-    private static Object sObject;
+    private static int ledongCount = 0;
+    private XSharedPreferences sharedPreferences;
 
     @Override
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
@@ -50,124 +45,47 @@ public class MainHook implements IXposedHookLoadPackage, IXposedHookZygoteInit {
         systemContext.registerReceiver(new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                isWeixin = intent.getExtras().getBoolean("weixin", true);
-                isQQ = intent.getExtras().getBoolean("qq", true);
-                m = Integer.valueOf(intent.getExtras().getString("magnification", "100"));
-                isAuto = intent.getExtras().getBoolean("autoincrement", false);
-                isLedong = intent.getExtras().getBoolean("ledong", true);
-                isYuedong = intent.getExtras().getBoolean("yuedong", true);
-                isPingan = intent.getExtras().getBoolean("pingan", true);
-                isCodoon = intent.getExtras().getBoolean("codoon", true);
-                isWeibo = intent.getExtras().getBoolean("weibo", true);
             }
         }, intentFilter);
-//        Thread autoThread = new Thread() {
-//            @Override
-//            public void run() {
-//                while (!isInterrupted()) {
-//                    try {
-//                        Thread.sleep(100);
-//                        if (sObject != null) {
-//                            count++;
-//                            XposedHelpers.callMethod(sObject, "dispatchSensorEvent", 5, new float[]{count, 0, 0}, 3, System.currentTimeMillis());
-//                        }
-//                        if (count == Integer.MAX_VALUE) {
-//                            count = 0;
-//                        }
-//                    } catch (InterruptedException e) {
-//                        e.printStackTrace();
-//                    }
-//
-//                }
-//            }
-//        };
-//        autoThread.start();
 
-        if (loadPackageParam.packageName.equals(WEIBO) || loadPackageParam.packageName.equals(PINGAN) || loadPackageParam.packageName.equals(WEXIN) || loadPackageParam.packageName.equals(QQ) || loadPackageParam.packageName.equals(LEDONG) || loadPackageParam.packageName.equals(YUEDONG) || loadPackageParam.packageName.equals(CODOON)) {
-            getKey();
-            final Class<?> sensorEL = XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue", loadPackageParam.classLoader);
-            XposedBridge.hookAllMethods(sensorEL, "dispatchSensorEvent", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
-                    int handle = (Integer) param.args[0];
-                    Field field = param.thisObject.getClass().getDeclaredField("mSensorsEvents");
-                    field.setAccessible(true);
-                    Sensor ss = ((SparseArray<SensorEvent>) field.get(param.thisObject)).get(handle).sensor;
-                    if (ss == null) {
-                        XposedBridge.log("传感器为NULL");
-                        return;
-                    }
-                    if (ss.getType() == Sensor.TYPE_ACCELEROMETER) {
-                        if (isLedong && loadPackageParam.packageName.equals(LEDONG)) {
-                            ledongCount += 1;
-                            //完美
-                            if (ledongCount % 3 == 0) {
-                                ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 100;
-                                ((float[]) param.args[1])[1] += (float) -10;
-                            } else if (ledongCount % 2 == 0) {
-                                ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
-                                ((float[]) param.args[1])[2] += (float) -20;
-                                ((float[]) param.args[1])[1] += (float) -5;
-                            } else {
-                                ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 10;
-                                ((float[]) param.args[1])[2] += (float) 20;
-                                ((float[]) param.args[1])[1] += (float) -15;
-                            }
-                        }
-                        if (isYuedong && loadPackageParam.packageName.equals(YUEDONG)) {
-                            yuedongCount += 1;
-                            ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
-                            if (yuedongCount % 2 == 0) {
-                                ((float[]) param.args[1])[2] += (float) -20;
-                                ((float[]) param.args[1])[1] += (float) -5;
-                            } else {
-                                ((float[]) param.args[1])[2] += (float) 20;
-                                ((float[]) param.args[1])[1] += (float) -15;
-                            }
-                        }
-                        if (isPingan && loadPackageParam.packageName.equals(PINGAN)) {
-                            pinganCount += 1;
-                            ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
-                            if (pinganCount % 2 == 0) {
-                                ((float[]) param.args[1])[2] += (float) -20;
-                                ((float[]) param.args[1])[1] += (float) -5;
-                            } else {
-                                ((float[]) param.args[1])[2] += (float) 20;
-                                ((float[]) param.args[1])[1] += (float) -15;
-                            }
-                        }
-                        if (isCodoon && loadPackageParam.packageName.equals(CODOON)) {
-                            codoonCount += 1;
-                            ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
-                            if (codoonCount % 2 == 0) {
-                                ((float[]) param.args[1])[2] += (float) -20;
-                                ((float[]) param.args[1])[1] += (float) -5;
-                            } else {
-                                ((float[]) param.args[1])[2] += (float) 20;
-                                ((float[]) param.args[1])[1] += (float) -15;
-                            }
-                        }
-                    }
-                    if (ss.getType() == Sensor.TYPE_STEP_COUNTER) {
-                        XposedHelpers.callMethod(param.thisObject, "dispatchSensorEvent", param.args[0], new float[]{((float[]) param.args[1])[0] + 1, 0, 0}, param.args[2], System.currentTimeMillis());
-                        XposedBridge.log(loadPackageParam.packageName + "，传感器类型：" + ss.getType());
-                    }
+        final Class<?> sensorEL = XposedHelpers.findClass("android.hardware.SystemSensorManager$SensorEventQueue", loadPackageParam.classLoader);
+        XposedBridge.hookAllMethods(sensorEL, "dispatchSensorEvent", new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(final MethodHookParam param) throws Throwable {
+                int handle = (Integer) param.args[0];
+                Field field = param.thisObject.getClass().getDeclaredField("mSensorsEvents");
+                field.setAccessible(true);
+                Sensor ss = ((SparseArray<SensorEvent>) field.get(param.thisObject)).get(handle).sensor;
+                if (ss == null) {
+                    XposedBridge.log("传感器为NULL");
+                    return;
                 }
-            });
-        }
+                if (ss.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    ledongCount += 1;
+                    //完美
+                    if (ledongCount % 3 == 0) {
+                        ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 100;
+                        ((float[]) param.args[1])[1] += (float) -10;
+                    } else if (ledongCount % 2 == 0) {
+                        ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
+                        ((float[]) param.args[1])[2] += (float) -20;
+                        ((float[]) param.args[1])[1] += (float) -5;
+                    } else {
+                        ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 10;
+                        ((float[]) param.args[1])[2] += (float) 20;
+                        ((float[]) param.args[1])[1] += (float) -15;
+                    }
+
+                }
+                if (ss.getType() == Sensor.TYPE_STEP_COUNTER) {
+                    ((float[]) param.args[1])[0] = ((float[]) param.args[1])[0] * 1000;
+                }
+            }
+        });
     }
 
     private void getKey() {
         sharedPreferences.reload();
-        isWeixin = sharedPreferences.getBoolean("weixin", true);
-        isQQ = sharedPreferences.getBoolean("qq", true);
-        m = Integer.valueOf(sharedPreferences.getString("magnification", "100"));
-        isAuto = sharedPreferences.getBoolean("autoincrement", false);
-        isLedong = sharedPreferences.getBoolean("ledong", true);
-        isYuedong = sharedPreferences.getBoolean("yuedong", true);
-        isPingan = sharedPreferences.getBoolean("pingan", true);
-        isCodoon = sharedPreferences.getBoolean("codoon", true);
-        isWeibo = sharedPreferences.getBoolean("weibo", true);
     }
 
     @Override
